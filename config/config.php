5033f2976380212db398950f5a5d32de8968abca
Achmad Nur Fauzi <61571729+FauziPay@users.noreply.github.com>
2022-05-19 11:44:52 +0700

    update file php

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_HOST", "sql6.freemysqlhosting.net");
+define("DB_USER", "sql6492932");
+define("DB_PASS", "qWa3NiK4it");
+define("DB_NAME", "sql6492932");
--- index.php
+++ index.php
@@ -1,36 +1,47 @@
+<?php include 'inc/header.php'; ?>
 <?php
-// Create database connection using config file
-include_once("config.php");
-
-// Fetch all users data from database
-$result = mysqli_query($mysqli, "SELECT * FROM skills ORDER BY id DESC");
+  Session::checkLogin();
 ?>
 
-<html>
 
-<head>
-    <title>Homepage</title>
-</head>
+	<div class="container">
+		<div class="row">
+        	<div class="col-lg-12 text-center">
+				<h1 class="mt-5">Online Examination System</h1>
+                <p class="lead">Simple MCQ Based Online Examination System!</p>
+				<img src="img/bgtest.png" width="130px;"/>				
+			</div>
 
-<body>
-    <a href="add.php">Add New User</a><br /><br />
+			<div class="col-lg-3">
+			
+			</div>
 
-    <table width='80%' border=1>
+			<div class="col-lg-6">
+				<form action="index.php" method="post"> 
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email Address</label>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Enter Email">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" name="password" id="password" class="form-control" placeholder="Password">
+					</div>
+					<button type="submit" id="loginsubm" value="Signup" class="btn btn-success">Log In</button>
+					</form>
+					<br/>
+					<p><a class="btn btn-outline-info btn-lg" href="register.php">New User? Signup for Free</a></p>
+					<span class="empty" style="display: none;">Fields must not be empty</span>
+					<span class="disable" style="display: none;">User ID Disable!</span>
+					<span class="error" style="display: none;">Email or Password did not match.</span>
+			</div>
 
-        <tr>
-            <th>Name</th>
-            <th>Mobile</th>
-            <th>Email</th>
-            <th>Update</th>
-        </tr>
-        <?php
-        while ($user_data = mysqli_fetch_array($result)) {
-            echo "<tr>";
-            echo "<td>" . $user_data['nama_skills'] . "</td>";
-            echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";
-        }
-        ?>
-    </table>
-</body>
 
-</html>
+			<div class="col-lg-3"></div>
+		</div>
+	</div>
+
+
+
+      
+	   
+<?php include 'inc/footer.php'; ?>
